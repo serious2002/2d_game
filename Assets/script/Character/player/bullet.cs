a44ac2de773f9e6963ba0fd9d2ea041be64ac5bd
nca-zszy <lijq256@mail2.sysu.edu.cn>
2024-12-26 21:53:39 +0800

    实现了射击功能，子弹的交互待完善

---
+++ Assets/script/Character/player/bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float lifetime;
+    private float shoottime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shoottime=Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > shoottime+lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/script/Character/player/playercontroller.cs
+++ Assets/script/Character/player/playercontroller.cs
@@ -5,25 +5,41 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject bullet;
+    public float bulletspeed=10;
     public float moveSpeed;
     private Rigidbody2D rb;
     public InputActions inputActions;
     public Vector2 inputDirection;
+    public Vector2 shootDirection;
+
+    [Header("射击间隔时间")]
+    public float shootInterval = 0.5f;  // 发射子弹的间隔，单位秒
+
+    private float lastShootTime = 0f;  // 记录上次发射子弹的时间
 
     public void PlayerHurt()
     {
-
+        
     }
 
     private void FixedUpdate()
     {
         rb.velocity = inputDirection * moveSpeed;
+
+        if (shootDirection != new Vector2(0.00f, 0.00f) && Time.time >= lastShootTime + shootInterval)
+        {
+            lastShootTime = Time.time;
+            GameObject newBullet = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody2D bulletRigidbody= newBullet.GetComponent<Rigidbody2D>();
+            bulletRigidbody.velocity = shootDirection * bulletspeed;
+        }
     }
 
     void Update()
     {
         inputDirection = inputActions.GamePlay.Move.ReadValue<Vector2>();
-
+        shootDirection = inputActions.GamePlay.Shoot.ReadValue<Vector2>();
     }
 
     private void Awake()
