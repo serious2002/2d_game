088b015fa9036837f86981273b478f2e2a3ef0a1
nca-zszy <lijq256@mail2.sysu.edu.cn>
2024-12-26 10:51:03 +0800

    输入系统更换，玩家代码已调整
    使用了InputSystem模块，在Settings文件夹的那个文件里可以绑定按键，更改后要点右边的apply重新生成对应代码

---
+++ Assets/script/AutoSetupCamera.cs
@@ -0,0 +1,11 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+//摄像机一运行就自动跟随玩家
+
+
+public class AutoSetupCamera : MonoBehaviour
+{
+
+}
---
+++ Assets/script/playermovement2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    public InputActions inputActions;
+    public Vector2 inputDirection;
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = inputDirection * moveSpeed;
+    }
+
+    void Update()
+    {
+        inputDirection = inputActions.GamePlay.Move.ReadValue<Vector2>();
+    }
+
+    private void Awake()
+    {
+        inputActions = new InputActions();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+}
+
