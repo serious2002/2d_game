e72ed9d0c06f1517a93e8e618290d720c5181a8e
serious2002 <2972444824@qq.com>
2024-12-26 21:03:25 +0800

    实现了一个模型人物的移动动画

--- Assets/script/playermovement.cs
+++ Assets/script/playermovement.cs
@@ -6,22 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     private Rigidbody2D rb;
+    Animator animator;
+    Vector2 moveDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
-    void Update()
+    private void FixedUpdate()
     {
+        //Debug.Log("FixedUpdate" + Time.deltaTime);
         // 获取玩家输入
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
+        // Debug.Log("moveX:" + moveX + ", moveY:" + moveY);
+
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
 
         // 计算移动方向
-        Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
+        moveDirection = new Vector2(moveX, moveY).normalized;
 
         // 应用移动
+       
         rb.velocity = moveDirection * moveSpeed;
     }
 }
