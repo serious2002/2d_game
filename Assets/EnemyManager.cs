54ba1fae3926391947c2439800fc27df44a5f621
nca-zszy <lijq256@mail2.sysu.edu.cn>
2024-12-30 18:12:14 +0800

    子弹优化
    现在子弹有了damage属性，人物有了playerdamage属性，敌人受到伤害时会读取子弹的伤害，playerdamage属性可以影响射出的子弹伤害，以方便实现单独提高人物伤害的操作

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -1,21 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
-{
-    [Header("敌人刷新点")] 
-    public Transform[] spawnPoints;
-    [Header("敌人巡逻点")] 
-    public Transform[] patrolPoints;
+//public class EnemyManager : MonoBehaviour
+//{
+//    [Header("敌人刷新点")] 
+//    public Transform[] spawnPoints;
+//    [Header("敌人巡逻点")] 
+//    public Transform[] patrolPoints;
 
-    [Header("该关卡的敌人")] 
-    public List<EnemyWave> enemyWaves;
-    public int currentWaveIndex = 0;//当前波数的索引
-    public int EnemyCount =0;//敌人数量
-    IEnumerator startNextWaveCoroutine()
-    {
-        if (currentWaveIndex >= enemyWaves.Count) yield break;//已经没有更多波数，直接退出协程
-        List<EnemyData> enemies = enemyWaves[currentWaveIndex].enemies;//获取当前波数对应的敌人列表
-    }
-}
+//    [Header("该关卡的敌人")] 
+//    public List<EnemyWave> enemyWaves;
+//    public int currentWaveIndex = 0;//当前波数的索引
+//    public int EnemyCount =0;//敌人数量
+//    IEnumerator startNextWaveCoroutine()
+//    {
+//        if (currentWaveIndex >= enemyWaves.Count) yield break;//已经没有更多波数，直接退出协程
+//        List<EnemyData> enemies = enemyWaves[currentWaveIndex].enemies;//获取当前波数对应的敌人列表
+//    }
+//}
