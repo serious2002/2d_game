0655ff0ada9c17dd85a6db904d89726fdb503613
C137A <153070485+C137A@users.noreply.github.com>
2025-01-02 22:35:18 +0800

    场景切换想法
    按钮切换和视频播放完切换，可以在场景开始时切入视频并关闭

--- Assets/Enemy/enemy.cs
+++ Assets/Enemy/enemy.cs
@@ -1,54 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.Events;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UIElements;
+//using UnityEngine.Events;
 
-public class enemy : MonoBehaviour
-{
-    public UnityEvent<Vector2> OnMovementInput;
+//public class enemy : MonoBehaviour
+//{
+//    public UnityEvent<Vector2> OnMovementInput;
 
-    public UnityEvent<Vector2> OnAttack;
+//    public UnityEvent<Vector2> OnAttack;
     
-    [SerializeField] private Transform : player
-    [SerializeField] private float chaseDistance = 3f; //×·»÷¾àÀë
+//    [SerializeField] private Transform : player
+//    [SerializeField] private float chaseDistance = 3f; //×·»÷¾àÀë
 
-    [SerializeField] private float attackDistance = 3f; //¹¥»÷¾àÀë
+//    [SerializeField] private float attackDistance = 3f; //¹¥»÷¾àÀë
 
-    private void Update()
-    {
-        if (player = null)
-            return;
-        float distance = Vector2 Distance(Player: position.transfom position);
+//    private void Update()
+//    {
+//        if (player = null)
+//            return;
+//        float distance = Vector2 Distance(Player: position.transfom position);
 
-        if(distance <= chaseDistance)//ÅÐ¶ÏÓëÍæ¼ÒµÄ¾àÀëÊÇ·ñÐ¡ÓÚµÈÓÚ×·»÷·¶Î§
-        {
-            if(distance <= attackDistance)//ÅÐ¶ÏÓëÍæ¼ÒµÄ¾àÀëÊÇ·ñÐ¡ÓÚµÈÓÚ¹¥»÷·¶Î§
-            {
-                //¹¥»÷Íæ¼Ò
+//        if(distance <= chaseDistance)//ÅÐ¶ÏÓëÍæ¼ÒµÄ¾àÀëÊÇ·ñÐ¡ÓÚµÈÓÚ×·»÷·¶Î§
+//        {
+//            if(distance <= attackDistance)//ÅÐ¶ÏÓëÍæ¼ÒµÄ¾àÀëÊÇ·ñÐ¡ÓÚµÈÓÚ¹¥»÷·¶Î§
+//            {
+//                //¹¥»÷Íæ¼Ò
       
-                OnMovementInput?.Invoke(Vector2.zero);//Í£Ö¹ÒÆ¶¯
+//                OnMovementInput?.Invoke(Vector2.zero);//Í£Ö¹ÒÆ¶¯
 
-                OnAttack?.Invoke();
-            }
-            else
-            {
-                // ×·»÷Íæ¼Ò
-                Vector2 direction = player.position - transform.position;
-                OnMovementInput?.Invoke(direction.normalized); // °ÑÒÆ¶¯·½Ïò´«¸øEnemyController
+//                OnAttack?.Invoke();
+//            }
+//            else
+//            {
+//                // ×·»÷Íæ¼Ò
+//                Vector2 direction = player.position - transform.position;
+//                OnMovementInput?.Invoke(direction.normalized); // °ÑÒÆ¶¯·½Ïò´«¸øEnemyController
 
-            }
+//            }
 
-        }
-        else
-        {
-            //Íæ¼ÒÍÑÀë×·»÷·¶Î§£¬·ÅÆú×·»÷
-            // ·ÅÆú×·»÷
-            OnMovementInput?.Invoke(Vector2.zero);
+//        }
+//        else
+//        {
+//            //Íæ¼ÒÍÑÀë×·»÷·¶Î§£¬·ÅÆú×·»÷
+//            // ·ÅÆú×·»÷
+//            OnMovementInput?.Invoke(Vector2.zero);
 
-        }
+//        }
 
-    }
+//    }
    
 
-}
+//}
