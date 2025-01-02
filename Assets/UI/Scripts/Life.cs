e94afd91c4a15931b34b1b981af86b568af9d49c
LiiiilWen <113098534+LiiiilWen@users.noreply.github.com>
2025-01-02 21:05:02 +0800

    第五关（scnes4）基本完成
    增加了音乐、npc对话和胜利失败条件，目前敌人仍为静止的（后续可再修改）所以设置了一些trap触碰扣血，血量扣到0游戏失败，敌人全部击杀游戏胜利

--- Assets/UI/Scripts/Life.cs
+++ Assets/UI/Scripts/Life.cs
@@ -17,13 +17,20 @@ public class Life : MonoBehaviour
 
 
     //待补充，受到伤害的条件并调用playDamaged（）以实现减少生命值
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Trap")
+        {
+            playerDamaged(1);
+        }
+    }
+
 
 
     //当角色受到伤害，可根据实际情况修改补充
-    //private void playerDamaged(int damage)
-    //{
-    //    currentLife -= damage;
-    //    Lifebar.setLifebar(currentLife);//在每次受伤时，更新生命值
-    //}
+    private void playerDamaged(int damage)
+    {
+        currentLife -= damage;
+        Lifebar.setLifebar(currentLife);//在每次受伤时，更新生命值
+    }
 }
